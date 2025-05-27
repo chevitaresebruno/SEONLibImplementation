fa34529743d4634d74add3d907c9f29127796ac9
chevitaresebruno <chevitarese.bruno@gmail.com>
2025-05-27 08:18:13 -0300

    Update: adicionando interfaces de uso com o usuario

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,33 @@
+import ClassAbstraction from "./abstractions/oo/ClassAbstraction";
 import PackageAbstraction from "./abstractions/project/PackageAbstraction";
 import ProjectAbstraction from "./abstractions/ProjectAbstraction";
+import PrimitiveTypeAbstraction from "./abstractions/types/PrimitiveTypeAbstraction";
+import { TypeScriptClass, TypescriptMethod } from "./prefabs/sintaxes/typescript/TSClass";
+import { TypescriptConstrains, TypescriptFile, TypescriptPackage } from "./prefabs/sintaxes/typescript/TSProject";
+import { TypescriptSintaxe } from "./prefabs/sintaxes/typescript/TSSintaxe";
+import { TypescriptBigInt, TypescriptBoolean, TypescriptNull, TypescriptNumber, TypescriptString, TypescriptUndefined } from "./prefabs/sintaxes/typescript/TSTypes";
+import { TypeScriptArggument, TypeScriptAttribute } from "./prefabs/sintaxes/typescript/TSVariables";
 import { vueModularArchProjectSettings } from "./prefabs/sintaxes/typescript/VueModularArchitecture";
 
 export default { 
     vueModularArchProjectSettings, 
-    ProjectAbstraction,
+    ProjectAbstraction, 
     PackageAbstraction,
+    PrimitiveTypeAbstraction,
+    TypeScriptAttribute,
+    TypeScriptClass,
+    ClassAbstraction,
+    TypeScriptArggument,
+    TypescriptBigInt,
+    TypescriptBoolean,
+    TypescriptString,
+    TypescriptConstrains,
+    TypescriptNull,
+    TypescriptNumber,
+    TypescriptUndefined,
+    TypescriptFile,
+    TypescriptMethod,
+    TypescriptSintaxe,
+    TypescriptPackage,
 };
   
