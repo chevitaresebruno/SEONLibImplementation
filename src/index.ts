4dfe7fbc453e17f68985ae9b590a9c9f958b043d
chevitaresebruno <chevitarese.bruno@gmail.com>
2025-06-03 08:48:48 -0300

    Update: tentando deixar o uso da lib como ARTEFATO no lugar de SEON.default.ARTEFATO

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,19 @@
-import ClassAbstraction from "./abstractions/oo/ClassAbstraction";
-import PackageAbstraction from "./abstractions/project/PackageAbstraction";
-import ProjectAbstraction from "./abstractions/ProjectAbstraction";
-import PrimitiveTypeAbstraction from "./abstractions/types/PrimitiveTypeAbstraction";
-import { TypeScriptClass, TypescriptMethod } from "./prefabs/sintaxes/typescript/TSClass";
-import { TypescriptConstrains, TypescriptFile, TypescriptPackage } from "./prefabs/sintaxes/typescript/TSProject";
-import { TypescriptSintaxe } from "./prefabs/sintaxes/typescript/TSSintaxe";
-import { TypescriptBigInt, TypescriptBoolean, TypescriptNull, TypescriptNumber, TypescriptString, TypescriptUndefined } from "./prefabs/sintaxes/typescript/TSTypes";
-import { TypeScriptArggument, TypeScriptAttribute } from "./prefabs/sintaxes/typescript/TSVariables";
-import { vueModularArchProjectSettings } from "./prefabs/sintaxes/typescript/VueModularArchitecture";
+export { default as ClassAbstraction } from "./abstractions/oo/ClassAbstraction";
+export { default as PackageAbstraction } from "./abstractions/project/PackageAbstraction";
+export { default as ProjectAbstraction } from "./abstractions/ProjectAbstraction";
+export { default as PrimitiveTypeAbstraction } from "./abstractions/types/PrimitiveTypeAbstraction";
 
-export default { 
-    vueModularArchProjectSettings, 
-    ProjectAbstraction, 
-    PackageAbstraction,
-    PrimitiveTypeAbstraction,
-    TypeScriptAttribute,
-    TypeScriptClass,
-    ClassAbstraction,
-    TypeScriptArggument,
-    TypescriptBigInt,
-    TypescriptBoolean,
-    TypescriptString,
-    TypescriptConstrains,
-    TypescriptNull,
-    TypescriptNumber,
-    TypescriptUndefined,
-    TypescriptFile,
-    TypescriptMethod,
-    TypescriptSintaxe,
-    TypescriptPackage,
-};
-  
+export { TypeScriptClass, TypescriptMethod } from "./prefabs/sintaxes/typescript/TSClass";
+export { TypescriptConstrains, TypescriptFile, TypescriptPackage } from "./prefabs/sintaxes/typescript/TSProject";
+export { TypescriptSintaxe } from "./prefabs/sintaxes/typescript/TSSintaxe";
+export {
+  TypescriptBigInt,
+  TypescriptBoolean,
+  TypescriptNull,
+  TypescriptNumber,
+  TypescriptString,
+  TypescriptUndefined
+} from "./prefabs/sintaxes/typescript/TSTypes";
+export { TypeScriptArggument, TypeScriptAttribute } from "./prefabs/sintaxes/typescript/TSVariables";
+export { vueModularArchProjectSettings } from "./prefabs/sintaxes/typescript/VueModularArchitecture";
+
